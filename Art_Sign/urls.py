8e7b5e164f636c151f14288a035caaa70d341914
Emeric Fischer <fischer.emeric@gmail.com>
2017-01-14 22:58:06 +0100

    Initial commit

---
+++ Art_Sign/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^', include('Art_Sign.pages.urls')),
+    url(r'^admin/', admin.site.urls),
+]
