96a61c26b71e130162723a3f853bdb0383b9d8f3
Emeric Fischer <fischer.emeric@gmail.com>
2017-01-17 22:49:03 +0100

    Mise en place de l'API

--- Art_Sign/settings.py
+++ Art_Sign/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'Art_Sign.pages',
     'Art_Sign.agenda',
 ]
