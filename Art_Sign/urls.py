d2172e15d43c8182ac5167564a478c1af20cb0f5
Myster <bodelmas@laposte.net>
2017-01-23 13:51:31 +0100

    Fix warning url + admin inacessible

--- Art_Sign/urls.py
+++ Art_Sign/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+	url(r'^', include('Art_Sign.pages.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('Art_Sign.api.urls')),
-	url(r'^', include('Art_Sign.pages.urls')),
 ]
