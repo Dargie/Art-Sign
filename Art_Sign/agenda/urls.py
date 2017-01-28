e30e18a3e61278be0eeb2843b56d0760ff27b19a
Emeric Fischer <fischer.emeric@gmail.com>
2017-01-28 17:53:14 +0100

    Ajout d'une vue pour lister les événements

--- Art_Sign/agenda/urls.py
+++ Art_Sign/agenda/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from Art_Sign.agenda.views import EventCreate
+from Art_Sign.agenda.views import EventListView, EventCreate
 
 urlpatterns = [
+    url(r'^', EventListView.as_view(), name='event-list'),
     url(r'^new/$', EventCreate.as_view(), name='event-create'),
 ]
 
--- Art_Sign/agenda/views.py
+++ Art_Sign/agenda/views.py
@@ -1,7 +1,13 @@
 from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
 
 from Art_Sign.agenda.forms import EventForm
 from Art_Sign.agenda.models import Event
+
+
+class EventListView(ListView):
+
+    model = Event
 
 
 class EventCreate(CreateView):
