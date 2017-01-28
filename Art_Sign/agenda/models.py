ea6fccf3ba5649d3ace682df735c12adb57af94b
Emeric Fischer <fischer.emeric@gmail.com>
2017-01-29 00:05:18 +0100

    Amélioration des modèles

--- Art_Sign/agenda/models.py
+++ Art_Sign/agenda/models.py
@@ -1,12 +1,29 @@
+from datetime import datetime
+
 from django.db import models
 
 
 class Event(models.Model):
 
-    title = models.CharField(max_length=80)
+    class Meta:
+        verbose_name = "Événement"
+        verbose_name_plural = "Événements"
 
-    date = models.DateTimeField()
+    title = models.CharField(max_length=80, verbose_name="Nom de l'événement")
+    date = models.DateTimeField(verbose_name="Date")
+    place = models.CharField(max_length=80, blank=True, verbose_name="Lieu")
+    description = models.CharField(max_length=200, blank=True, verbose_name="Description")
 
-    place = models.CharField(max_length=80, blank=True)
+    def __str__(self):
+        return self.name
 
-    description = models.CharField(max_length=200)
+    def is_past(self):
+        """
+        Test if the event's date is past.
+        :return: True id the event's date is past, False if it's not.
+        :rtype: bool
+        """
+        if self.date > datetime.now():
+            return True
+        elif self.date < datetime.now():
+            return False
