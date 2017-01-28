c1d3044e86831adf4a52b28fdc89caf3a6ffe11b
Emeric Fischer <fischer.emeric@gmail.com>
2017-01-28 18:03:07 +0100

    Fix bug URL

--- Art_Sign/agenda/urls.py
+++ Art_Sign/agenda/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from Art_Sign.agenda.views import EventListView, EventCreate
 
 urlpatterns = [
-    url(r'^', EventListView.as_view(), name='event-list'),
+    url(r'^$', EventListView.as_view(), name='event-list'),
     url(r'^new/$', EventCreate.as_view(), name='event-create'),
 ]
 
