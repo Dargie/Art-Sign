237b86f098bf2e1c406ace827ef0809f84c58743
Emeric Fischer <fischer.emeric@gmail.com>
2017-02-20 19:58:25 +0100

    Ajout de nouveaux tests

--- Art_Sign/agenda/tests.py
+++ Art_Sign/agenda/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 
+from Art_Sign.agenda.factories import EventFactory
 from Art_Sign.agenda.models import Event
 
 
@@ -7,6 +8,9 @@ class EventModelTest(TestCase):
     """
     Test the event model.
     """
+    def setUp(self):
+
+        self.event = EventFactory()
 
     def test_string_representation(self):
         """
@@ -14,3 +18,10 @@ class EventModelTest(TestCase):
         """
         event = Event(title="Assemblée générale !")
         self.assertEqual(str(event), event.title)
+        self.assertEqual(str(self.event), self.event.title)
+
+    def test_is_past(self):
+        """
+        Test the is_past method.
+        """
+        self.assertEqual(self.event.is_past(), False)
