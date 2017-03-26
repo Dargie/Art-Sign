b2ee23193b41ac346d966c7239036b415c44b448
Emeric Fischer <fischer.emeric@gmail.com>
2017-03-26 17:39:02 +0200

    API : uniquement les articles publiés.

--- Art_Sign/article/api/views.py
+++ Art_Sign/article/api/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 from Art_Sign.article.api.serializers import ArticleListSerializer, ArticleDetailSerializer
@@ -6,12 +8,12 @@ from Art_Sign.article.models import Article
 
 class ArticleListAPI(ListAPIView):
 
-    queryset = Article.objects.all()
+    queryset = Article.objects.exclude(pub_date__gte=datetime.now())
     serializer_class = ArticleListSerializer
 
 
 class ArticleDetailAPI(RetrieveAPIView):
 
-    queryset = Article.objects.all()
+    queryset = Article.objects.exclude(pub_date__gte=datetime.now())
     lookup_url_kwarg = 'article_id'
     serializer_class = ArticleDetailSerializer
