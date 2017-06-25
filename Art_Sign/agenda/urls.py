685ce786683463544c9d885827d9e74a38521ec0
Emeric Fischer <fischer.emeric@gmail.com>
2017-06-25 16:11:57 +0200

    Ajout d'une page détail pour les évenements.

--- Art_Sign/agenda/urls.py
+++ Art_Sign/agenda/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from Art_Sign.agenda.views import EventListView
+from Art_Sign.agenda.views import EventListView, EventDetailView
 
 urlpatterns = [
     url(r'^$', EventListView.as_view(), name='event-list'),
+    url(r'^(?P<pk>[-\w]+)/$', EventDetailView.as_view(), name='event-detail'),
 ]
--- Art_Sign/agenda/views.py
+++ Art_Sign/agenda/views.py
@@ -1,9 +1,15 @@
+from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
 from Art_Sign.agenda.models import Event
 
 
 class EventListView(ListView):
+
+    model = Event
+
+
+class EventDetailView(DetailView):
 
     model = Event
 
