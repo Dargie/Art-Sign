b02c54dcf6482c44b666e0d79fb88022abcdc714
Emeric Fischer <fischer.emeric@gmail.com>
2017-06-25 18:02:57 +0200

    Affiche les derniers articles sur la home

--- Art_Sign/pages/views.py
+++ Art_Sign/pages/views.py
@@ -3,13 +3,16 @@ import datetime
 from django.shortcuts import render
 
 from Art_Sign.agenda.models import Event
+from Art_Sign.article.models import Article
 
 
 def home(request):
     today = datetime.date.today()
     events = Event.objects.order_by('-date').filter(date__gte=today)
+    articles = Article.objects.order_by('-pub_date')
 
     return render(request, 'pages/home.html', {
         'next_events': events,
+        'last_articles': articles,
     })
 
