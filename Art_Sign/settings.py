d194b64a12463a24b134c40cf88cf2148918a1d6
Emeric Fischer <fischer.emeric@gmail.com>
2017-06-26 19:32:11 +0200

    Static files + base template

--- Art_Sign/settings.py
+++ Art_Sign/settings.py
@@ -124,9 +124,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/public/'
 
-STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'public/'),
+)
 
 MEDIA_URL = '/'
 
