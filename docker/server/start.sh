35f851b2faeef08fbf36b557a16ce468d157539e
EpicKiwi <lelycandu76@gmail.com>
2017-07-14 15:33:34 +0200

    Watch de gulp et démarrage avec le serveur

--- docker/server/start.sh
+++ docker/server/start.sh
@@ -2,11 +2,11 @@
 
 cd /code
 
-if [ -e /firstrun.tag ]; then
+if [ ! -e /code/db.sqlite3 ]; then
 	python manage.py migrate
 	python manage.py loaddata fixtures/*.yaml
 	rm -f /firstrun.tag
 fi
 
-echo "Starting Django server on *:8000"
-python manage.py runserver 0.0.0.0:8000
+echo "Starting Django server on *:8000 and Gulp watch task"
+python manage.py runserver 0.0.0.0:8000 & gulp watch
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,25 @@
-var gulp = require("gulp");
+var gulp = require("gulp")
 var sass = require("gulp-sass")
+var sourcemaps = require("gulp-sourcemaps")
+
+var path = {
+	sass: {
+		source: "./public/sass/**/*.scss",
+		destination: "./public/sass",
+		map: "."
+	}
+}
 
 gulp.task('sass', () => {
-	gulp.src('public/sass/**/*.scss')
+	return gulp.src(path.sass.source)
+	.pipe(sourcemaps.init())
 	.pipe(sass().on('error',sass.logError))
-	.pipe(gulp.dest('public/sass'))
+	.pipe(sourcemaps.write(path.sass.map))
+	.pipe(gulp.dest(path.sass.destination))
+})
+
+gulp.task('default', ['sass'] , () => {})
+
+gulp.task('watch', () => {
+	gulp.watch(path.sass.source, ['sass'])
 })
